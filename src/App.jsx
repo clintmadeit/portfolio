518c0dc0677933ca033ac7d2419a7af24ba4c436
clintmadeit <clintonbrirdley@gmail.com>
2023-11-22 16:59:47 +0300

    Project initialization and adding packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,5 @@
-import "./app.scss";
-import NavBar from "./components/navbar/NavBar";
-
 const App = () => {
-  return (
-    <div>
-      <section>
-        <NavBar />
-      </section>
-      <section>Parallax</section>
-      <section>Services</section>
-      <section>Parallax</section>
-      <section>Portfolio1</section>
-      <section>Portfolio2</section>
-      <section>Portfolio3</section>
-      <section>Contact</section>
-    </div>
-  );
+  return <div>Hello World</div>;
 };
 
 export default App;
