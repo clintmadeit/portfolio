720b5f6ba4264f9840dbf3860eaa393f9c2121e8
clintmadeit <clintonbrirdley@gmail.com>
2023-11-23 17:11:17 +0300

    Hero and navbar implementation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,18 +2,18 @@ import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
 import { motion } from "framer-motion";
 
-const Navbar = () => {
+export default function Navbar() {
   return (
     <div className="navbar">
       {/* Sidebar */}
-      <Sidebar/>
+      <Sidebar />
       <div className="wrapper">
         <motion.span
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
         >
-          Lama Dev
+          <img className="logo" src="/clint.png" alt="Clint Dev" />
         </motion.span>
         <div className="social">
           <a href="#">
@@ -26,12 +26,10 @@ const Navbar = () => {
             <img src="/youtube.png" alt="" />
           </a>
           <a href="#">
-            <img src="/dribbble.png" alt="" />
+            <img src="/dribble.png" alt="" />
           </a>
         </div>
       </div>
     </div>
   );
-};
-
-export default Navbar;
+}
--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -2,31 +2,30 @@ import { motion } from "framer-motion";
 
 const variants = {
   open: {
-    transition: {
-      staggerChildren: 0.1,
-    },
+    transition: { staggerChildren: 0.1, delayChildren: 0.2 },
   },
+
   closed: {
-    transition: {
-      staggerChildren: 0.05,
-      staggerDirection: -1,
-    },
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
   },
 };
+
 const itemVariants = {
   open: {
     y: 0,
     opacity: 1,
+    transition: { y: { stiffness: 1000, velocity: -100 } },
   },
+
   closed: {
     y: 50,
     opacity: 0,
+    transition: { y: { stiffness: 1000 } },
   },
 };
 
 const Links = () => {
-  const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
-
+  const items = ["Home", "About", "Services", "Portfolio", "Contact"];
   return (
     <motion.div className="links" variants={variants}>
       {items.map((item) => (
@@ -34,8 +33,8 @@ const Links = () => {
           href={`#${item}`}
           key={item}
           variants={itemVariants}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
+          whileHover={{ scale: 1.1, fontWeight: "bold", color: "#0c0c1d" }}
+          whileTap={{ scale: 0.75 }}
         >
           {item}
         </motion.a>
