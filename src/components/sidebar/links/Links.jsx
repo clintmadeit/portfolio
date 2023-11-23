070fa80adb4a6299e21e6dcf0d1064b9d8553e33
clintmadeit <clintonbrirdley@gmail.com>
2023-11-23 12:01:06 +0300

    Styling the sidebar toggle behavior

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,13 +1,45 @@
+import { motion } from "framer-motion";
+
+const variants = {
+  open: {
+    transition: { staggerChildren: 0.1, delayChildren: 0.2 },
+  },
+
+  closed: {
+    transition: { staggerChildren: 0.05, staggerDirection: -1 },
+  },
+};
+
+const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: { y: { stiffness: 1000, velocity: -100 } },
+  },
+
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: { y: { stiffness: 1000 } },
+  },
+};
+
 const Links = () => {
   const items = ["Home", "About", "Services", "Portfolio", "Contact"];
   return (
-    <div className="links">
+    <motion.div className="links" variants={variants}>
       {items.map((item) => (
-        <a href={`#${item}`} key={item}>
+        <motion.a
+          href={`#${item}`}
+          key={item}
+          variants={itemVariants}
+          whileHover={{ scale: 1.1, fontWeight: "bold", color: "#0c0c1d" }}
+          whileTap={{ scale: 0.75 }}
+        >
           {item}
-        </a>
+        </motion.a>
       ))}
-    </div>
+    </motion.div>
   );
 };
 
